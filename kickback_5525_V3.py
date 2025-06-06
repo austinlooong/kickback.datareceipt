20b3cea4a17089457a54002659ac0320a5758574
austinlooong <austinlooong@gmail.com>
2025-06-06 17:39:43 -0400

    Update kickback_5525_V3.py

--- kickback_5525_V3.py
+++ kickback_5525_V3.py
@@ -138,9 +138,6 @@ if zip_file:
     with tempfile.TemporaryDirectory() as tmpdir:
         with zipfile.ZipFile(zip_file, "r") as z:
             z.extractall(tmpdir)
-            watch_path = None
-            search_path = None
-
 
         watch_path = None
         search_path = None
